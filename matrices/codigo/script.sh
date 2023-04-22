c826616f251d4c2de1763ca70bd528058efb6e8d
egruttner <egruttner@udec.cl>
2023-04-22 17:39:54 -0400

    Ajuste

--- matrices/codigo/script.sh
+++
@@ -1,4 +0,0 @@
-rm main.o
-make
-
-#python3 plot.py standard_mm_results.csv transpose_mm_results.csv strassen_mm_results.csv
--- ordenamiento/codigo/script.sh
+++
@@ -1,4 +0,0 @@
-rm main.o
-make
-
-python3 plot.py selection_mm_results.csv bubble_mm_results.csv quicksort_mm_results.csv sortinterno_mm_results.csv mergesort_mm_results.csv
